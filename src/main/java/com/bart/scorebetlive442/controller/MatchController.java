c102b90a0fdac58a386db2fe987251fe6a74ee63
bartoszkolodziej <bartosz.kolodziej@comeon.com>
2024-09-24 15:40:31 +0200

    some changes in Match controller

--- src/main/java/com/bart/scorebetlive442/controller/MatchController.java
+++ src/main/java/com/bart/scorebetlive442/controller/MatchController.java
@@ -2,7 +2,10 @@ package com.bart.scorebetlive442.controller;
 
 import com.bart.scorebetlive442.mapper.MatchMapper;
 import com.bart.scorebetlive442.model.Match;
+import com.bart.scorebetlive442.model.json.LeagueJson;
+import com.bart.scorebetlive442.model.json.MatchJson;
 import com.bart.scorebetlive442.service.MatchService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,43 +27,52 @@ public class MatchController {
         this.matchMapper = matchMapper;
     }
 
-    @PostMapping(value = "/create")
-    public ResponseEntity<MatchResponseJson> createTeam(@RequestBody MatchCreateJson matchCreateJson) {
+    @PostMapping
+    @JsonView(MatchJson.View.CreateResponse.class)
+    public ResponseEntity<MatchJson> createTeam(@RequestBody @JsonView(MatchJson.View.CreateRequest.class)
+                                                    MatchJson matchJson) {
 
-        Match match = matchMapper.convertJsonToMatch(matchCreateJson);
-        Match createdMatch = matchService.createMatch(match);
-        MatchResponseJson matchResponse = matchMapper.convertMatchToJson(createdMatch);
-        return new ResponseEntity<>(matchResponse, HttpStatus.CREATED);
+        Match createdMatch = matchService.createMatch(matchMapper.convertJsonToMatch(matchJson));
+        return new ResponseEntity<>(matchMapper.convertMatchToJson(createdMatch), HttpStatus.CREATED);
     }
 
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<MatchResponseJson> getTeam(@PathVariable(name = "id") Long matchId) {
+    @JsonView(MatchJson.View.GetResponse.class)
+    public ResponseEntity<MatchJson> getTeam(@PathVariable(name = "id") Long matchId) {
         Match match = matchService.getMatch(matchId);
 
         if (match != null) {
-            MatchResponseJson matchResponse = matchMapper.convertMatchToJson(match);
-            return new ResponseEntity<>(matchResponse, HttpStatus.OK);
+            return new ResponseEntity<>(matchMapper.convertMatchToJson(match), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping(value = "/all")
-    public ResponseEntity<List<MatchResponseJson>> getAllMatches() {
+    @JsonView(LeagueJson.View.GetResponse.class)
+    public ResponseEntity<List<MatchJson>> getAllMatches() {
         List<Match> allMatches = matchService.getAllMatches();
 
         if (allMatches.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
-            List<MatchResponseJson> matchResponse = allMatches.stream()
+            List<MatchJson> matchResponse = allMatches.stream()
                     .map(matchMapper::convertMatchToJson)
                     .collect(Collectors.toList());
             return new ResponseEntity<>(matchResponse, HttpStatus.OK);
         }
     }
 
-    @DeleteMapping(value = "/remove/{id}")
+    @PatchMapping(value = "/{id}")
+    @JsonView(LeagueJson.View.GetResponse.class)
+    public ResponseEntity<MatchJson> updateTeam(@PathVariable Long id, @RequestBody @JsonView(MatchJson.View.CreateRequest.class)
+    MatchJson matchJson) {
+        Match updatedMatch = matchService.updateMatch(id, matchMapper.convertJsonToMatch(matchJson));
+        return new ResponseEntity<>(matchMapper.convertMatchToJson(updatedMatch), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteTeam(@PathVariable Long id) {
         boolean isMatchRemoved = matchService.removeMatchById(id);
         if (isMatchRemoved) {
@@ -68,12 +80,5 @@ public class MatchController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-    }
-
-    @PatchMapping(value = "/update/{id}")
-    public ResponseEntity<MatchResponseJson> updateTeam(@PathVariable Long id, @RequestBody MatchCreateJson bodyMatch) {
-        Match match = matchMapper.convertJsonToMatch(bodyMatch);
-        Match updatedMatch = matchService.updateMatch(id, match);
-        return new ResponseEntity<>(matchMapper.convertMatchToJson(updatedMatch), HttpStatus.OK);
     }
 }
